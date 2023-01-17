7f83c8d8ed7fdffe46048aac1657ecb9a99245ef
unknown <yury.ovchinnkov@yandex.ru>
2023-01-17 18:43:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { DatePicker, Input, Rate } from "antd";
+
+const App = () => {
+  return (
+    <>
+      <DatePicker />
+      <Input placeholder="Test input" />
+      <Rate />
+    </>
+  );
+};
+
+export default App;
